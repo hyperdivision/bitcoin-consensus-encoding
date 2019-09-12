9443fc087edc0b676121e370ab8c987238236b65
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-09-12 11:45:24 +0200

    directory restructured

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 module.exports = {
-  boolean: require('./boolean'),
-  string: require('./string'),
-  varInt: require('./var-int')
+  boolean: require('./lib/boolean'),
+  command: require('./lib/command'),
+  fvi: require('./lib/flag-var-int'),
+  hash: require('./lib/hash'),
+  hashmap: require('./lib/hashmap'),
+  int: require('./lib/int'),
+  message: require('./lib/message'),
+  outpoint: require('./lib/outpoint'),
+  script: require('./lib/script'),
+  string: require('./lib/string'),
+  txout: require('./lib/tx-out')
+  varint: require('./lib/var-int')
 }
