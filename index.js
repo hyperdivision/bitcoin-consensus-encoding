e8043e1cffa463e244d6ac4fb5fd2701045e532f
Christophe Diederichs <chris.d.itunes@gmail.com>
2019-09-13 20:07:38 +0200

    new encoding types added to exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   boolean: require('./lib/boolean'),
+  bytes: require('./lib/byte-string'),
   command: require('./lib/command'),
   fvi: require('./lib/flag-var-int'),
   hash: require('./lib/hash'),
@@ -7,7 +8,9 @@ module.exports = {
   int: require('./lib/int'),
   message: require('./lib/message'),
   outpoint: require('./lib/outpoint'),
+  pubkey: require('./lib/pubkey')
   script: require('./lib/script'),
+  signature: require('./lib/ecdsa-sig.js'),
   string: require('./lib/string'),
   txout: require('./lib/tx-out'),
   varint: require('./lib/var-int')
